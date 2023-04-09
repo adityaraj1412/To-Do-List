bdec7acdf08d66899828b78ae873f48563b6f83f
Aditya Raj <82364388+adityaraj1412@users.noreply.github.com>
2023-04-09 15:47:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const bodyParser = require("body-parser")
+const express=require("express")
+const app=express();
+let items=["Buy Food","Go Shopping","Play Cricket"]
+
+app.set('view engine','ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.get("/",function(req,res){
+    let today=new Date();
+    let options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    }
+    let day=today.toLocaleDateString("en-US",options)
+
+res.render("lists",{
+    kindofDay:day,newItemList:items
+})
+})
+app.post("/",function(req,res){
+    let item=req.body.newItem;
+    items.push(item);
+    res.redirect("/")
+})
+app.listen(4000);
